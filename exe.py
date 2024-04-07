9f9ef389878737947f82b902b28f7d1dcfc272a0
m2kism2k <125483583+m2kism2k@users.noreply.github.com>
2024-04-07 10:45:17 +0630

    Update exe.py

--- exe.py
+++ exe.py
@@ -1,1 +1,10 @@
+order_list = [2, 4, 5, 6]
+price=4.55
+amount_list = []
+for i in oreder_list:
+  amount =0
+  amount = i * price
+  amount_format = "{:.2f}".format(amount)
+  amount_list.append(float(amount_format))
 
+print("Amount List : "+str(amount_list))
